75e3f71bf3f0a2e8b79018aad97b85800fb7fb5a
Ruben <ruben.msalles@gmail.com>
2024-06-07 16:50:33 -0400

    📝 (404.html): añadir página de error 404 para el sitio web ✨ (tarjeta-pokemon.component.html): añadir imagen y número de Pokémon en la tarjeta ♻️ (tarjeta-pokemon.component.ts): refactorizar componente para mostrar información del Pokémon 📝 (pokeapi.ts): añadir interfaces para los datos de la API de Pokémon ♻️ (home.component.html): refactorizar estructura del componente home ♻️ (home.component.scss): refactorizar estilos del componente home ♻️ (home.component.ts): refactorizar componente para cargar lista de Pokémon ✅ (pokemon.service.spec.ts): añadir pruebas unitarias para el servicio de Pokémon ✨ (pokemon.service.ts): añadir servicio para obtener datos de la API de Pokémon

--- src/app/components/tarjeta-pokemon/tarjeta-pokemon.component.ts
+++ src/app/components/tarjeta-pokemon/tarjeta-pokemon.component.ts
@@ -1,11 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, Input, OnChanges, SimpleChanges } from '@angular/core';
+import { CommonModule, TitleCasePipe } from '@angular/common';
+import { Resultado } from '../../interfaces/pokeapi';
+import { PokemonService } from '../../services/pokemon.service';
 
 @Component({
   selector: 'app-tarjeta-pokemon',
   standalone: true,
   templateUrl: './tarjeta-pokemon.component.html',
-  styleUrls: ['./tarjeta-pokemon.component.scss']
+  styleUrls: ['./tarjeta-pokemon.component.scss'],
+  imports: [CommonModule, TitleCasePipe]
 })
-export class TarjetaPokemonComponent {
+export class TarjetaPokemonComponent implements OnChanges {
 
+  @Input() data?: Resultado;
+  id: string = "0";
+
+  constructor(private pokemonService: PokemonService) { }
+
+  ngOnChanges(changes: SimpleChanges): void {
+    this.extraerInformacion();
+  }
+
+  extraerInformacion() {
+    if (this.data && this.data.url) {
+      this.id = this.data.url.split("/")[6];
+    }
+  }
 }
--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,14 +1,32 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { FotoPokemonComponent } from './../../components/foto-pokemon/foto-pokemon.component';
 import { TarjetaPokemonComponent } from './../../components/tarjeta-pokemon/tarjeta-pokemon.component';
+import { PokemonService } from '../../services/pokemon.service';
+import { CommonModule, TitleCasePipe } from '@angular/common';
+import { Resultado } from '../../interfaces/pokeapi'; 
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [FotoPokemonComponent, TarjetaPokemonComponent],
+  imports: [FotoPokemonComponent, TarjetaPokemonComponent, CommonModule, TitleCasePipe],
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss']
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit {
 
+  cargando: boolean = true; // Define la propiedad cargando
+
+  listaPokemon: Resultado[] = [];
+
+  constructor(private pokemonService: PokemonService) { }
+
+  ngOnInit(): void {
+    this.cargarLista();
+  }
+
+  async cargarLista() {
+    this.listaPokemon = [...this.listaPokemon, ...await this.pokemonService.getByPage()];
+    this.cargando = false; // Actualiza cargando después de cargar la lista
+    console.log(this.listaPokemon);
+  }
 }
